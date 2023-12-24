f00d772761e75c36e01e578690d06c0690b11863
Tanjilhossaintamim <tanjilhossain845@gmail.com>
2023-12-24 09:26:46 +0600

    added skeleton

--- src/pages/home/product/Product.jsx
+++ src/pages/home/product/Product.jsx
@@ -1,17 +1,33 @@
 import Heading from "../../../components/shared/Heading/Heading";
 import ProductCard from "../../../components/shared/ProductCard/ProductCard";
+import LoadingSkeleton from "../../../components/shared/Skeleton/Skeleton";
+
 import { useGetAllProductsQuery } from "../../../redux/features/product/productApi";
 
 const Product = () => {
-  const { data } = useGetAllProductsQuery();
+  const { data, isLoading } = useGetAllProductsQuery();
+
+  let content = null;
+  if (isLoading) {
+    content = (
+      <>
+        <LoadingSkeleton />
+        <LoadingSkeleton />
+        <LoadingSkeleton />
+      </>
+    );
+  }
+  if (data?.payload?.length > 0) {
+    content = data?.payload.map((product) => (
+      <ProductCard key={product._id} product={product} />
+    ));
+  }
 
   return (
     <div className="max-w-7xl mx-auto py-10">
       <Heading />
       <div className="grid grid-cols-1 place-content-center md:grid-cols-2 lg:grid-cols-3 gap-6 py-16">
-        {data?.payload.map((product) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
+        {content}
       </div>
     </div>
   );
